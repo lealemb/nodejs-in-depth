9c680682bb8137b7ab10d51733776ce68db964cd
lealemb <lealemb84@gmail.com>
2024-11-03 23:18:50 +0300

    write file

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -8,6 +8,12 @@ fs.readFile(path.join(__dirname,'starter.txt'), 'utf8', (err, data) => {
 
 console.log("Hello .....");
 
+fs.writeFile(path.join(__dirname,'reply.txt'), 'Nice to meet you',(err) => {
+    if (err) throw err;
+    console.log('write complete');
+})
+
+
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`);
     process.exit(1); 
