f75b80eba46ca451b95a73381a7f7527fc148a32
lealemb <lealemb84@gmail.com>
2024-11-06 00:23:45 +0300

    Revert "new change"
    This reverts commit 183ec4da9faf7cb28f68d7fcd4ec09d1e9ce47fc.

---
+++ 01TUT/math.js
@@ -0,0 +1,7 @@
+exports.add =(a,b) => a + b;
+exports.subtract =(a,b) => a - b;
+exports.multiplay =(a,b) => a * b;
+exports.divide =(a,b) => a / b;
+
+
+// module.exports = {add, subtract, divide, multiplay};
