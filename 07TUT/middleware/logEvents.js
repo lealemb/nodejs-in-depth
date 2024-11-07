b2032fb44923deaa3f23766926946ddd09755958
lealemb <lealemb84@gmail.com>
2024-11-07 21:41:40 +0300

    server.js modified

--- 07TUT/middleware/logEvents.js
+++ 07TUT/middleware/logEvents.js
@@ -1,10 +1,11 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
+
 const fs = require('fs');
 const fsPromises = require('fs').promises;  
 const path = require('path');
 
-    const logEvents = async (message) => {
+    const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
@@ -14,7 +15,7 @@ const path = require('path');
         if (!fs.existsSync(path.join(__dirname,'..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', 'eventLog.txt'), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
     }
