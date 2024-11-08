9d482114b36c646692607152f097c1591a46b8d3
lealemb <lealemb84@gmail.com>
2024-11-08 22:54:53 +0300

    file added

--- 07TUT/server.js
+++ 07TUT/server.js
@@ -13,7 +13,7 @@ app.use(logger);
 const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
 const corsOptions ={
     origin:(origin, callback) => {
-        if(whitelist.indexOf(origin) !== -1 || !origin){
+        if(whitelist.indexOf(origin) !== -1 ||!origin){
             callback(null, true);
         }else {
             callback(new Error('Not allowed by CORS'));
