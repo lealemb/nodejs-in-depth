7b89a014a8e75870ee98c660597364dd29241ad8
lealemb <lealemb84@gmail.com>
2024-11-13 21:12:37 +0300

    mvc added

---
+++ 09TUT/controllers/employeesController.js
@@ -0,0 +1,31 @@
+const data = {};
+data.employees = require('../../data/employees.json');
+
+const getAllEmployees = (req, res)=>{
+    res.json(data.employees); 
+}
+createNewEmployee = (req, res)=>{
+    res.json({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName
+    });
+}
+const updateEmployee = (req, res)=>{
+    res.json({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName
+    });
+}
+const deleteEmployee = (req, res)=>{
+    res.json({"id":req.body.id});
+}
+const getEmployee = (req, res)=>{
+    res.json({"id":req.params.id});
+}
+module.exports = {
+    getAllEmployees,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee
+}
