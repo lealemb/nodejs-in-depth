0a112e0e7ddc82b8835b632eaa99211dabeecf76
lealemb <lealemb84@gmail.com>
2024-11-18 21:49:02 +0300

    new package added

--- 10TUT/controllers/registerController.js
+++ 10TUT/controllers/registerController.js
@@ -1,1 +1,11 @@
-const registerController = require('../controllers/registerController');
+const userDB = {
+    users : require('../model/users.json'),
+    setUser: function(data) {this.users = data}
+}
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const {user,pwd } = rew.body;
+}
