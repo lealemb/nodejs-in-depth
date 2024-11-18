99c9ed79289a2d471f3651e3a8ac9984820db60b
lealemb <lealemb84@gmail.com>
2024-11-18 22:56:51 +0300

    new

--- 10TUT/controllers/registerController.js
+++ 10TUT/controllers/registerController.js
@@ -1,11 +1,18 @@
-const userDB = {
-    users : require('../model/users.json'),
-    setUser: function(data) {this.users = data}
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
 }
 const fsPromises = require('fs').promises;
 const path = require('path');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
-    const {user,pwd } = rew.body;
-}
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    // check for duplicate usernames in the db
+    const duplicate = usersDB.users.find(person => person.username === user);
+    if (duplicate) return res.sendStatus(409); //Conflict 
+    try {
+   
+
+module.exports = { handleNewUser };
