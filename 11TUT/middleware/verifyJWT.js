936121d7649498d0b17af9a4bc8a97ea67caa0c8
lealemb <lealemb84@gmail.com>
2024-11-22 22:28:49 +0300

    new file

--- 11TUT/middleware/verifyJWT.js
+++ 11TUT/middleware/verifyJWT.js
@@ -1,5 +1,18 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const verifyJWT = (req, res, next) =>{
+
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-}
+    if (!authHeader) return res.sendStatus(401); // No Authorization header
+
+    console.log(authHeader); // Logs "Bearer token"
+    const token = authHeader.split(' ')[1]; // Extracts token
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403); // Invalid token
+        req.user = decoded.username; // Store decoded username in req.user
+        next(); // Proceed to next middleware
+    });
+};
+
+module.exports = verifyJWT;
