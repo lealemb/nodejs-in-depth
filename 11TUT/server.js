31430a641a7ab5b5ee0918e1836d227ccfa4f4ed
lealemb <lealemb84@gmail.com>
2024-11-24 11:49:39 +0300

    new

--- 11TUT/server.js
+++ 11TUT/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-
+const verifyJWT = require('./middleware/verifyJWT');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
@@ -25,6 +25,8 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require ('./routes/root'));
 app.use('/register', require ('./routes/register'));
 app.use('/auth', require ('./routes/auth'));
+
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 
