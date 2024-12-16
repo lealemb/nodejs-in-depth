bc1057a28f0067bab9571edaa1c171855c1bdb33
lealemb <lealemb84@gmail.com>
2024-12-16 15:35:03 +0300

    new

--- 11TUT/server.js
+++ 11TUT/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-const {logger} = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
@@ -18,7 +18,7 @@ app.use(credentials);
 //cross origin resourse shariging
 app.use(cors(corsOptions));
 //built-in middleware to handle urlencoded form data
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //built-in middleware for json 
 app.use(express.json());
@@ -30,29 +30,27 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 //routes
-app.use('/', require ('./routes/root'));
-app.use('/register', require ('./routes/register'));
-app.use('/auth', require ('./routes/auth'));
-app.use('/refresh', require ('./routes/refresh'));
-app.use('/logOut', require ('./routes/logOut'));
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logOut', require('./routes/logOut'));
 
 
 app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
-
-
 //app.use('/')
-app.all('/*', (req, res) =>{
+app.all('/*', (req, res) => {
     res.status(404);
-    if(req.accepts('html')){
+    if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
-    }else if(req.accepts('json')){
-        res.json({error: '404 Not found'});
-    }else {
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not found' });
+    } else {
         res.type('txt').send("404 Not found");
     }
 });
 app.use(errorHandler);
- 
-app.listen(PORT, ()=> console.log(`server running on ${PORT}`));
- //time 20  13
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
+//time 20  13
